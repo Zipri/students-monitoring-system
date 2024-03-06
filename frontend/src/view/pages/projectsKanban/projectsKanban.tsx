5ada90574f4894ed296785da1fa1701d6558bd60
Seroshtan_KV <seroshtan_kv@rtsoft.ru>
2024-03-06 11:09:49 +0300

    front: Добавление библиотек, создание простого перетаскивания

--- frontend/src/view/pages/projectsKanban/projectsKanban.tsx
+++ frontend/src/view/pages/projectsKanban/projectsKanban.tsx
@@ -1,15 +1,50 @@
 import { useStores } from '@control';
 import { useEffect } from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
 const ProjectsKanban = () => {
   const { projectsKanban } = useStores();
-  const { getUserProjects } = projectsKanban;
+  const { userProjects, userInfo, getUserProjects } = projectsKanban;
+
+  const onDragEnd = (result: any) => {
+    console.log(result);
+  };
 
   useEffect(() => {
     getUserProjects();
   }, []);
 
-  return <div></div>;
+  return (
+    <div>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {userProjects.map((project, index) => (
+                <Draggable
+                  key={project.id}
+                  draggableId={project.id}
+                  index={index}
+                >
+                  {(provided) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      {/* Здесь ваш контент для перетаскивания */}
+                      {project.title}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
 };
 
 export default ProjectsKanban;
