43b48345f7f292876a7fa72933b2f6d83c81b472
Seroshtan_KV <seroshtan_kv@rtsoft.ru>
2024-03-17 17:37:25 +0300

    react-calendar-timeline

--- frontend/src/control/stores/timelineTasks/timelineTasks.ts
+++ frontend/src/control/stores/timelineTasks/timelineTasks.ts
@@ -1,11 +1,11 @@
 import { action, makeObservable, observable } from 'mobx';
+import { TProject } from 'model/api/projects/types';
 import { TTask } from 'model/api/tasks/types';
 import { TasksService } from 'model/services/tasks';
 
 import { TUid } from '@api/types';
 import { Loading } from '@stores/common';
 import { StoreManager } from '@stores/manager';
-import { TProject } from 'model/api/projects/types';
 
 class TimelineTasksStore {
   private tasksService!: TasksService;
--- frontend/src/view/pages/timelineTasks/timeline/timeline.tsx
+++ frontend/src/view/pages/timelineTasks/timeline/timeline.tsx
@@ -1,4 +1,5 @@
 import { observer } from 'mobx-react-lite';
+import Timeline from 'react-calendar-timeline';
 
 import { useStores } from '@control';
 
@@ -6,17 +7,29 @@ const TimelineTasksPart = () => {
   const { timelineTasks, manager } = useStores();
   const { projectTasks, currentProject } = timelineTasks;
 
+  const groups = projectTasks.map((task) => ({
+    id: task.id,
+    title: task.title,
+  }));
+
+  const items = projectTasks.map((task) => ({
+    id: task.id,
+    group: task.id,
+    title: task.title,
+    start_time: new Date(task.startDate),
+    end_time: new Date(task.deadline),
+  }));
+
+  if (!currentProject) return <></>;
+
   return (
     <div>
-      {projectTasks.map((i) => i.status)}
-      <br />
-      {currentProject?.status}
-      {/* <Timeline
+      <Timeline
         groups={groups}
         items={items}
-        defaultTimeStart={moment().add(-12, 'hour')}
-        defaultTimeEnd={moment().add(12, 'hour')}
-      /> */}
+        defaultTimeStart={new Date(currentProject.startDate)}
+        defaultTimeEnd={new Date(currentProject.deadline)}
+      />
     </div>
   );
 };
