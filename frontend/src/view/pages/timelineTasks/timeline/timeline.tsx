590608ef8e6eba936a301a8ddc5b4538fb41077f
Seroshtan_KV <seroshtan_kv@rtsoft.ru>
2024-03-20 17:18:05 +0300

    fix

--- frontend/src/view/pages/timelineTasks/timeline/timeline.tsx
+++ frontend/src/view/pages/timelineTasks/timeline/timeline.tsx
@@ -1,6 +1,7 @@
 import {
   eachDayOfInterval,
   format,
+  isSameDay,
   isWithinInterval,
   parseISO,
 } from 'date-fns';
@@ -32,26 +33,6 @@ const TimelineTasksPart = () => {
     }
     return acc;
   }, []);
-
-  const isTaskInInterval = (day: Date, task: TTask) =>
-    isWithinInterval(day, {
-      start: parseISO(task.startDate),
-      end: parseISO(task.deadline),
-    });
-
-  const getTaskDaysPerMonth = (task: TTask, months: string[], days: Date[]) => {
-    const taskStart = parseISO(task.startDate);
-    const taskEnd = parseISO(task.deadline);
-    return months.map((monthYear) => {
-      const monthDays = days.filter(
-        (day) => format(day, 'yyyy-MM') === monthYear
-      );
-      const taskDaysInMonth = monthDays.filter((day) =>
-        isWithinInterval(day, { start: taskStart, end: taskEnd })
-      );
-      return taskDaysInMonth.length;
-    });
-  };
 
   useHorizontalScroll('timeline-tasks-part');
 
@@ -90,83 +71,44 @@ const TimelineTasksPart = () => {
         </thead>
 
         <tbody>
-          {projectTasks.map((task) => (
-            <tr key={task.id}>
-              {months.map((monthYear) => {
-                // Получаем дни текущего месяца
-                const monthDays = days.filter(
-                  (day) => format(day, 'yyyy-MM') === monthYear
-                );
-                // Начало и конец задачи
-                const taskStart = parseISO(task.startDate);
-                const taskEnd = parseISO(task.deadline);
-                // Проверяем, попадает ли задача в текущий месяц
-                const isTaskInMonth = monthDays.some((day) =>
-                  isWithinInterval(day, { start: taskStart, end: taskEnd })
-                );
+          {projectTasks.map((task) => {
+            const taskStart = parseISO(task.startDate);
+            const taskEnd = parseISO(task.deadline);
+            const taskStartIndex = days.findIndex((day) =>
+              isSameDay(day, taskStart)
+            );
+            const taskEndIndex = days.findIndex((day) =>
+              isSameDay(day, taskEnd)
+            );
+            const taskLength = taskEndIndex - taskStartIndex + 1;
 
-                if (!isTaskInMonth) {
-                  // Если задача не попадает в месяц, рендерим пустые ячейки для всего месяца
-                  return monthDays.map((_, index) => (
-                    <td
-                      key={`${monthYear}-${index}`}
-                      className={styles.td}
-                    ></td>
-                  ));
-                } else {
-                  // Иначе, определяем начальный и конечный индексы задачи в месяце
-                  const startIndex = monthDays.findIndex(
-                    (day) => day >= taskStart
-                  );
-                  const endIndex = monthDays.findIndex((day) => day > taskEnd);
-                  const colspan =
-                    endIndex === -1
-                      ? monthDays.length - startIndex
-                      : endIndex - startIndex;
-
-                  // Рендерим пустые ячейки перед задачей, если необходимо
-                  const emptyCellsBefore =
-                    startIndex > 0
-                      ? monthDays
-                          .slice(0, startIndex)
-                          .map((_, index) => (
-                            <td
-                              key={`${monthYear}-empty-before-${index}`}
-                              className={styles.td}
-                            ></td>
-                          ))
-                      : [];
-
-                  // Рендерим ячейку задачи с соответствующим colSpan
-                  const taskCell = (
-                    <td
-                      key={`${monthYear}-task-${task.id}`}
-                      colSpan={colspan}
-                      className={classNames(styles.td, styles.filledTd)}
-                      style={colorSchema[task.status as TaskStatusEnum].content}
-                    >
-                      {task.title}
-                    </td>
-                  );
-
-                  // Рендерим пустые ячейки после задачи, если необходимо
-                  const emptyCellsAfter =
-                    endIndex !== -1
-                      ? monthDays
-                          .slice(endIndex)
-                          .map((_, index) => (
-                            <td
-                              key={`${monthYear}-empty-after-${index}`}
-                              className={styles.td}
-                            ></td>
-                          ))
-                      : [];
-
-                  return [...emptyCellsBefore, taskCell, ...emptyCellsAfter];
-                }
-              })}
-            </tr>
-          ))}
+            return (
+              <tr key={task.id}>
+                {days.map((day, index) => {
+                  if (index === taskStartIndex) {
+                    return (
+                      <td
+                        key={index}
+                        colSpan={taskLength}
+                        className={classNames(styles.td, styles.filledTd)}
+                        style={
+                          colorSchema[task.status as TaskStatusEnum].content
+                        }
+                      >
+                        {task.title}
+                      </td>
+                    );
+                  } else if (index > taskStartIndex && index <= taskEndIndex) {
+                    // Пропускаем ячейки внутри диапазона задачи
+                    return null;
+                  } else {
+                    // Пустые ячейки за пределами задачи
+                    return <td key={index} className={styles.td}></td>;
+                  }
+                })}
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
