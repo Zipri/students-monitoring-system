5e921522b5c42ea7cd019a8b4d77a9e19a8b2c24
Seroshtan_KV <seroshtan_kv@rtsoft.ru>
2024-03-20 20:51:39 +0300

    ещё стилей

--- frontend/src/view/pages/timelineTasks/timeline/timeline.tsx
+++ frontend/src/view/pages/timelineTasks/timeline/timeline.tsx
@@ -1,22 +1,18 @@
-import {
-  eachDayOfInterval,
-  format,
-  isSameDay,
-  isWithinInterval,
-  parseISO,
-} from 'date-fns';
+import { eachDayOfInterval, format, isSameDay, parseISO } from 'date-fns';
 import { observer } from 'mobx-react-lite';
-import { TaskStatusEnum, TTask } from 'model/api/tasks/types';
+import { TaskStatusEnum } from 'model/api/tasks/types';
+import { Button } from 'primereact/button';
 import { classNames } from 'primereact/utils';
 
-import { tasksKanbanColorSchema } from '@config';
+import { tasksKanbanColorSchema, tasksPriorityColorSchema } from '@config';
 import { useStores } from '@control';
-import { getBackendDate, useHorizontalScroll } from '@view/utils';
+import { EllipsisText } from '@view/common';
+import { useHorizontalScroll } from '@view/utils';
 
 import styles from './styles.module.scss';
-import { EllipsisText } from '@view/common';
 
 const colorSchema = tasksKanbanColorSchema;
+const colorSchema2 = tasksPriorityColorSchema;
 
 const TimelineTasksPart = () => {
   const { timelineTasks, manager } = useStores();
@@ -41,7 +37,7 @@ const TimelineTasksPart = () => {
 
   if (!currentProject) return <div id="timeline-tasks-part"></div>;
   return (
-    <div className={styles.wrapper} id="timeline-tasks-part">
+    <div className={styles.wrapper}>
       <table className={styles.infoTable}>
         <thead>
           <tr>
@@ -60,6 +56,13 @@ const TimelineTasksPart = () => {
               >
                 <div className={styles.staticContent}>
                   <EllipsisText>{task.title}</EllipsisText>
+                  <Button
+                    text
+                    tooltip="Открыть"
+                    tooltipOptions={{ position: 'top' }}
+                    icon="pi pi-external-link"
+                    onClick={() => {}}
+                  />
                 </div>
               </td>
             </tr>
@@ -67,90 +70,119 @@ const TimelineTasksPart = () => {
         </tbody>
       </table>
 
-      <table className={styles.table}>
-        <thead>
-          <tr>
-            {months.map((monthYear) => {
-              const monthDays = days.filter(
-                (day) => format(day, 'yyyy-MM') === monthYear
-              );
-              return (
+      <div id="timeline-tasks-part" className={styles.tableWrapper}>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              {months.map((monthYear) => {
+                const monthDays = days.filter(
+                  (day) => format(day, 'yyyy-MM') === monthYear
+                );
+                return (
+                  <th
+                    key={monthYear}
+                    colSpan={monthDays.length}
+                    className={styles.th}
+                  >
+                    {format(parseISO(`${monthYear}-01`), 'MMMM yyyy')}
+                  </th>
+                );
+              })}
+            </tr>
+            <tr>
+              {days.map((day) => (
                 <th
-                  key={monthYear}
-                  colSpan={monthDays.length}
-                  className={styles.th}
+                  key={day.toString()}
+                  className={classNames(styles.th, {
+                    [styles.today]: isSameDay(day, today),
+                  })}
+                  style={{ minWidth: '10rem' }}
                 >
-                  {format(parseISO(`${monthYear}-01`), 'MMMM yyyy')}
+                  <div className="flex align-items-center justify-content-center gap-2">
+                    {isSameDay(day, today) && <div>Сегодня</div>}
+                    {format(day, 'dd')}
+                  </div>
                 </th>
+              ))}
+            </tr>
+          </thead>
+
+          <tbody>
+            {projectTasks.map((task) => {
+              const taskStart = parseISO(task.startDate);
+              const taskEnd = parseISO(task.deadline);
+              const taskStartIndex = days.findIndex((day) =>
+                isSameDay(day, taskStart)
+              );
+              const taskEndIndex = days.findIndex((day) =>
+                isSameDay(day, taskEnd)
+              );
+              const taskLength = taskEndIndex - taskStartIndex + 1;
+
+              return (
+                <tr key={task.id}>
+                  {days.map((day, index) => {
+                    if (index === taskStartIndex) {
+                      return (
+                        <td
+                          key={index}
+                          colSpan={taskLength}
+                          className={classNames(styles.td, styles.filledTd)}
+                          style={
+                            colorSchema[task.status as TaskStatusEnum].content
+                          }
+                        >
+                          <div className={styles.content}>
+                            <Button
+                              outlined
+                              icon="pi pi-angle-left"
+                              tooltip="Уменьшить на день"
+                              tooltipOptions={{ position: 'top' }}
+                            />
+                            <div className="flex align-items-center justify-content-center gap-2">
+                              <div className={styles.label}>
+                                <EllipsisText>{task.title}</EllipsisText>
+                              </div>
+                              <div
+                                className={styles.label}
+                                style={colorSchema2[task.priority].content}
+                              >
+                                {task.priority}
+                              </div>
+                            </div>
+                            <Button
+                              outlined
+                              icon="pi pi-angle-right"
+                              tooltip="Увеличить на день"
+                              tooltipOptions={{ position: 'top' }}
+                            />
+                          </div>
+                        </td>
+                      );
+                    } else if (
+                      index > taskStartIndex &&
+                      index <= taskEndIndex
+                    ) {
+                      // Пропускаем ячейки внутри диапазона задачи
+                      return null;
+                    } else {
+                      // Пустые ячейки за пределами задачи
+                      return (
+                        <td
+                          key={index}
+                          className={classNames(styles.td, {
+                            [styles.todayTd]: isSameDay(day, today),
+                          })}
+                        ></td>
+                      );
+                    }
+                  })}
+                </tr>
               );
             })}
-          </tr>
-          <tr>
-            {days.map((day) => (
-              <th
-                key={day.toString()}
-                className={classNames(styles.th, {
-                  [styles.today]: isSameDay(day, today),
-                })}
-                style={{ minWidth: '10rem' }}
-              >
-                <div className="flex align-items-center justify-content-center gap-2">
-                  {isSameDay(day, today) && <div>Сегодня</div>}
-                  {format(day, 'dd')}
-                </div>
-              </th>
-            ))}
-          </tr>
-        </thead>
-
-        <tbody>
-          {projectTasks.map((task) => {
-            const taskStart = parseISO(task.startDate);
-            const taskEnd = parseISO(task.deadline);
-            const taskStartIndex = days.findIndex((day) =>
-              isSameDay(day, taskStart)
-            );
-            const taskEndIndex = days.findIndex((day) =>
-              isSameDay(day, taskEnd)
-            );
-            const taskLength = taskEndIndex - taskStartIndex + 1;
-
-            return (
-              <tr key={task.id}>
-                {days.map((day, index) => {
-                  if (index === taskStartIndex) {
-                    return (
-                      <td
-                        key={index}
-                        colSpan={taskLength}
-                        className={classNames(styles.td, styles.filledTd)}
-                        style={
-                          colorSchema[task.status as TaskStatusEnum].content
-                        }
-                      >
-                        <div className={styles.content}>{task.title}</div>
-                      </td>
-                    );
-                  } else if (index > taskStartIndex && index <= taskEndIndex) {
-                    // Пропускаем ячейки внутри диапазона задачи
-                    return null;
-                  } else {
-                    // Пустые ячейки за пределами задачи
-                    return (
-                      <td
-                        key={index}
-                        className={classNames(styles.td, {
-                          [styles.todayTd]: isSameDay(day, today),
-                        })}
-                      ></td>
-                    );
-                  }
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
